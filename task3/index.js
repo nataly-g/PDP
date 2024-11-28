74e75dd18ad85c9ab0a42d36d126d316c36e45a9
Nataly <60840574+nataly-g@users.noreply.github.com>
2024-11-28 11:02:54 +0200

    Add files via upload

---
+++ task3/index.js
@@ -0,0 +1,10 @@
+import Library from './library.js';
+const library = new Library();
+library.add('My Brilliant Friend', 'Elena Ferrante', 2012);
+library.add('The Warmth of Other Suns', 'Isabel Wilkerson', 2010);
+library.list();
+library.search('hobbit');
+library.search('ANYWAYS');
+library.remove('suns');
+library.checkAvailability('Larsson');
+library.list();
