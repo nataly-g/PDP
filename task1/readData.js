e0ac250b8c81a3c6edcafc2af72cac444d5b301a
Nataly <60840574+nataly-g@users.noreply.github.com>
2024-12-23 22:16:18 +0200

    Delete task1 directory

--- task1/readData.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-
-fs.readFile('infoFile.txt', 'utf8', (error, data) => {
-  if (error) {
-    console.error('Error reading file:', error);
-    return;
-  }
-  console.log(data);
-});
